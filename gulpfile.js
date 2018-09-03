300879bafb7c3fc25ca15328ed2c4c8bae0ba1a7
wangxiang <212590236@qq.com>
2018-09-03 09:14:26 +0800

    onr

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var server = require('gulp-webserver');
+var uglify = require('gulp-uglify');
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+var css = require('gulp-clean-css');
+
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/*.scss')
+        .pipe(sass())
+        .pipe(css())
+        .pipe(gulp.dest('./src/css/'))
+});
+gulp.task('watch', function() {
+    return gulp.watch('./src/scss/*.scss', gulp.series('sass'));
+});
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 8080,
+            middleware: function(req, res) {
+                var pathname = url.parse(req.url).pathname;
+                if (req.url === '/favicon.ico') {
+                    res.end('');
+                    return;
+                }
+                pathname = pathname === '/' ? '/index.html' : pathname;
+                res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+            }
+        }))
+})
+gulp.task('dev', gulp.series('sass', 'server', 'watch'));
